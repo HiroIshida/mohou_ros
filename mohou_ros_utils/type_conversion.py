3fe60f10da87e3c2fae890d569d2915979f51056
HiroIshida <spitfire.docomo@gmail.com>
2022-03-06 07:01:09 +0900

    Simplify type conversion

--- mohou_ros_utils/type_conversion.py
+++ mohou_ros_utils/type_conversion.py
@@ -1,46 +1,54 @@
-from typing import Optional
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
 
 from sensor_msgs.msg import Image
+import genpy
 import numpy as np
 from cv_bridge import CvBridge
+from mohou.types import ElementT
 from mohou.types import RGBImage
 from mohou.types import DepthImage
-from mohou.types import ElementSequence
 
-from mohou_ros_utils.types import TimeStampedSequence
 from mohou_ros_utils.resizer import RGBResizer
 from mohou_ros_utils.resizer import DepthResizer
 
 
-def Image_to_RGBImage(msg: Image, resizer: RGBResizer) -> RGBImage:
-    bridge = CvBridge()
-    image = bridge.imgmsg_to_cv2(msg, desired_encoding='rgb8')
-    data = resizer(image)
-    return RGBImage(data)
+MessageT = TypeVar('MessageT', bound=genpy.Message)
 
 
-def Image_to_DepthImage(msg: Image, resizer: DepthResizer) -> DepthImage:
-    size = [msg.height, msg.width]
-    buf: np.ndarray = np.ndarray(shape=(1, int(len(msg.data) / 4)), dtype=np.float32, buffer=msg.data)
-    depth = np.nan_to_num(buf.reshape(*size))
-    data = np.expand_dims(resizer(depth), axis=2)
-    return DepthImage(data)
+class TypeConverter(ABC, Generic[MessageT, ElementT]):
+
+    @abstractmethod
+    def __call__(self, msg: MessageT) -> ElementT:
+        pass
 
 
-def sequence_to_element_sequence(
-        seq: TimeStampedSequence,
-        rgb_resizer: Optional[RGBResizer] = None,
-        depth_resizer: Optional[DepthResizer] = None) -> ElementSequence:
-    if seq.object_type == Image:
-        image_encoding = seq.object_list[0].encoding  # type: ignore
-        if image_encoding in ['bgr8', 'rgb8']:
-            assert rgb_resizer is not None
-            elem_seq = ElementSequence([Image_to_RGBImage(msg, rgb_resizer) for msg in seq.object_list])  # type: ignore
-        elif seq.object_list[0].encoding in ['32FC1']:  # type: ignore
-            assert depth_resizer is not None
-            elem_seq = ElementSequence([Image_to_DepthImage(msg, depth_resizer) for msg in seq.object_list])  # type: ignore
-        else:
-            raise NotImplementedError(image_encoding)
-    else:
-        raise NotImplementedError
-    return elem_seq
+class RGBImageConverter(TypeConverter[Image, RGBImage]):
+    resizer: RGBResizer
+
+    def __init__(self, resizer: RGBResizer):
+        self.resizer = resizer
+
+    def __call__(self, msg: Image) -> RGBImage:
+        assert msg.encoding in ['bgr8', 'rgb8']
+
+        bridge = CvBridge()
+        image = bridge.imgmsg_to_cv2(msg, desired_encoding='rgb8')
+        data = self.resizer(image)
+        return RGBImage(data)
+
+
+class DepthImageConverter(TypeConverter[Image, DepthImage]):
+    resizer: DepthResizer
+
+    def __init__(self, resizer: DepthResizer):
+        self.resizer = resizer
+
+    def __call__(self, msg: Image) -> DepthImage:
+        assert msg.encoding in ['32FC1']
+
+        size = [msg.height, msg.width]
+        buf: np.ndarray = np.ndarray(shape=(1, int(len(msg.data) / 4)), dtype=np.float32, buffer=msg.data)
+        depth = np.nan_to_num(buf.reshape(*size))
+        data = np.expand_dims(self.resizer(depth), axis=2)
+        return DepthImage(data)
