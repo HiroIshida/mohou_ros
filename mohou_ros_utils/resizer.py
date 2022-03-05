c00cbda0d5b1f319deae55f09fed4ef5ed90dbd5
HiroIshida <spitfire.docomo@gmail.com>
2022-03-06 07:09:23 +0900

    Use cv2 interpolation for depth

--- mohou_ros_utils/resizer.py
+++ mohou_ros_utils/resizer.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 import numpy as np
-import scipy.interpolate
 import cv2
 
 
@@ -29,20 +28,8 @@ class RGBResizer(AbstractResizer):
 
 class DepthResizer(AbstractResizer):
 
-    def __call__(self, image) -> np.ndarray:
+    def __call__(self, image, method=cv2.INTER_CUBIC) -> np.ndarray:
 
         image_resized = image[self.x_bound, self.y_bound]
-
-        def create_mesh_points(width, height):
-            xlin = np.linspace(0, 1., width)
-            ylin = np.linspace(0, 1., height)
-            xx, yy = np.meshgrid(xlin, ylin)
-            pts = np.array(list(zip(xx.flatten(), yy.flatten())))
-            return pts
-
-        pts_in = create_mesh_points(*image_resized.shape)
-        pts_out = create_mesh_points(self.resol, self.resol)
-
-        itped = scipy.interpolate.griddata(pts_in, image_resized.T.flatten(), pts_out)
-        itped_reshaped = itped.reshape(self.resol, self.resol).T
-        return itped_reshaped
+        image_final = cv2.resize(image_resized, (self.resol, self.resol), interpolation=method)
+        return image_final
