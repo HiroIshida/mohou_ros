f273e0aa3a24c0c66e966d14882c2861acd26f40
HiroIshida <spitfire.docomo@gmail.com>
2022-03-05 15:46:10 +0900

    Add more test

--- tests/test_synclonizer.py
+++ tests/test_synclonizer.py
@@ -5,7 +5,7 @@ from mohou_ros_utils.synclonizer import TimeStampedSequence
 from mohou_ros_utils.synclonizer import synclonize
 
 
-def test_synclonize():
+def test_synclonize_case1():
     N = 6
     times = np.array([float(i) for i in range(N)])
     times2 = times + 0.2
@@ -24,3 +24,15 @@ def test_synclonize():
     seq1_new, seq2_new = seqs_new
     assert abs(seq1_new.time_list[0] - 2.0) < 1e-4
     assert abs(seq1_new.time_list[-1] - N) < 1e-4
+
+
+def test_synclonize_case2():
+    N = 11
+    times = np.array([float(i) * 1.0 for i in range(N)])
+    times2 = np.array([float(i) * 1.2 for i in range(N)])
+    seq1 = TimeStampedSequence([None for _ in range(N)], times)
+    seq2 = TimeStampedSequence([None for _ in range(N)], times2)
+
+    freq = 2.0
+    seqs_new = synclonize([seq1, seq2], freq=freq)
+    assert len(seqs_new[0].time_list) == 6
