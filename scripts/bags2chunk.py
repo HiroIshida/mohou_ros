b9eaa0256be92d8a4550ebcf0795e0e65413672e
HiroIshida <spitfire.docomo@gmail.com>
2022-05-15 00:39:25 +0900

    feat: dump chunk for image autoencoder

--- scripts/bags2chunk.py
+++ scripts/bags2chunk.py
@@ -7,7 +7,7 @@ import rospkg
 from typing import List
 from moviepy.editor import ImageSequenceClip
 from mohou.file import get_project_dir
-from mohou.types import RGBImage, AngleVector
+from mohou.types import RGBImage, AngleVector, ImageBase
 from mohou.types import MultiEpisodeChunk, EpisodeData, ElementSequence
 
 from mohou_ros_utils.types import TimeStampedSequence
@@ -48,7 +48,8 @@ def has_too_long_static_av(edata: EpisodeData, coef: float = 0.1):
     return indices_static > static_state_len_threshold
 
 
-def main(config: Config, hz: float, dump_gif: bool):
+def main(config: Config, hz: float, dump_gif: bool, for_image_autoencoder: bool):
+    postfix = 'image_autoencoder' if for_image_autoencoder else None
     rosbag_dir = get_rosbag_dir(config.project)
     episode_data_list = []
     for filename_ in os.listdir(rosbag_dir):
@@ -64,22 +65,28 @@ def main(config: Config, hz: float, dump_gif: bool):
 
         filename = os.path.join(rosbag_dir, filename_)
 
+        topic_name_list = config.topics.use_topic_list
+        if for_image_autoencoder:
+            def fn(topic: str) -> bool:
+                tc = config.topics.name_config_table[topic]
+                return issubclass(tc.mohou_type, ImageBase)
+            topic_name_list = list(filter(fn, topic_name_list))
+        print('topic_list: {}'.format(topic_name_list))
+
         rule = AllSameInterpolationRule(NearestNeighbourMessageInterpolator)
         bag = rosbag.Bag(filename)
-        seqs = bag_to_synced_seqs(bag,
-                                  1.0 / hz,
-                                  topic_names=config.topics.use_topic_list,
-                                  rule=rule)
+        seqs = bag_to_synced_seqs(bag, 1.0 / hz, topic_names=topic_name_list, rule=rule)
         bag.close()
 
         episode_data = seqs_to_episodedata(seqs, config)
-        if has_too_long_static_av(episode_data):
+        if not for_image_autoencoder and has_too_long_static_av(episode_data):
             print('skipped (strange too long initial static state found. skipped)')
             continue
         episode_data_list.append(episode_data)
 
-    chunk = MultiEpisodeChunk.from_data_list(episode_data_list)
-    chunk.dump(config.project)
+    extra_info = {'hz': hz}
+    chunk = MultiEpisodeChunk.from_data_list(episode_data_list, extra_info=extra_info)
+    chunk.dump(config.project, postfix)
 
     if dump_gif:
         gif_dir = os.path.join(get_project_dir(config.project), 'train_data_gifs')
@@ -89,7 +96,10 @@ def main(config: Config, hz: float, dump_gif: bool):
             images = [rgb.numpy() for rgb in episode_data.get_sequence_by_type(RGBImage)]
             clip = ImageSequenceClip(images, fps=fps)
 
-            gif_filename = os.path.join(gif_dir, '{}.gif'.format(i))
+            if postfix is None:
+                gif_filename = os.path.join(gif_dir, '{}.gif'.format(i))
+            else:
+                gif_filename = os.path.join(gif_dir, '{}-{}.gif'.format(i, postfix))
             clip.write_gif(gif_filename, fps=fps)
 
 
@@ -98,10 +108,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-config', type=str, default=os.path.join(config_dir, 'pr2_rarm.yaml'))
     parser.add_argument('-hz', type=float, default=5.0)
+    parser.add_argument('--image_autoencoder', action='store_true', help='chunk for image autoencoder')
     parser.add_argument('--gif', action='store_true', help='dump gifs for debugging')
 
     args = parser.parse_args()
     config = Config.from_yaml_file(args.config)
     hz = args.hz
     dump_gif = args.gif
-    main(config, hz, dump_gif)
+    for_image_autoencoder = args.image_autoencoder
+    main(config, hz, dump_gif, for_image_autoencoder)
