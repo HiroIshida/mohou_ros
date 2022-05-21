6105a5ff8ff3252407fe8eec7059736653a36dd2
HiroIshida <spitfire.docomo@gmail.com>
2022-05-22 01:46:54 +0900

    feat: add CoordinateTransfom

--- mohou_ros_utils/utils.py
+++ mohou_ros_utils/utils.py
@@ -1,5 +1,8 @@
 import warnings
 import functools
+import numpy as np
+from dataclasses import dataclass
+from typing import Optional
 
 
 def deprecated(func):
@@ -13,3 +16,35 @@ def deprecated(func):
         warnings.simplefilter('default', DeprecationWarning)  # reset filter
         return func(*args, **kwargs)
     return new_func
+
+
+@dataclass
+class CoordinateTransform:
+    trans: np.ndarray
+    rot: np.ndarray
+    src: Optional[str] = None
+    dest: Optional[str] = None
+
+    def __call__(self, vec_src: np.ndarray) -> np.ndarray:
+        return self.rot.dot(vec_src) + self.trans
+
+    def inverse(self) -> 'CoordinateTransform':
+        rot_new = self.rot.T
+        trans_new = -rot_new.dot(self.trans)
+        return CoordinateTransform(trans_new, rot_new, self.dest, self.src)
+
+    @classmethod
+    def from_src_coordinate_wrt_dest(cls, pos, rot) -> 'CoordinateTransform':
+        return CoordinateTransform(pos, rot).inverse()
+
+
+def chain_transform(tf_a2b: CoordinateTransform, tf_b2c: CoordinateTransform) -> CoordinateTransform:
+    if tf_a2b.dest is not None and tf_b2c.src is not None:
+        assert tf_a2b.dest == tf_b2c.src
+
+    trans_a2c = tf_b2c.trans + tf_b2c.rot.dot(tf_a2b.trans)
+    rot_a2c = tf_b2c.rot.dot(tf_a2b.rot)
+
+    src_new = tf_a2b.src
+    dest_new = tf_b2c.dest
+    return CoordinateTransform(trans_a2c, rot_a2c, src_new, dest_new)
---
+++ tests/test_utils.py
@@ -0,0 +1,21 @@
+from skrobot.coordinates.math import rotation_matrix_from_rpy
+import math
+import numpy as np
+
+from mohou_ros_utils.utils import chain_transform, CoordinateTransform
+
+
+def test_transform():
+    mat = rotation_matrix_from_rpy([math.pi * 0.25, 0.0, 0.0])
+    tf_b2a = CoordinateTransform(np.array([1, 0, 0]), mat, 'b', 'a')
+    tf_c2b = CoordinateTransform(np.array([1, 0, 0]), mat, 'c', 'b')
+    tf_c2a = chain_transform(tf_c2b, tf_b2a)
+
+    trans = np.array([1.0 + 0.5 * np.sqrt(2.0), 0.5 * np.sqrt(2.0), 0.0])
+    np.testing.assert_almost_equal(tf_c2a.trans, trans, decimal=8)
+    np.testing.assert_almost_equal(tf_c2a.rot, mat.dot(mat), decimal=8)
+
+    tf_a2c = tf_c2a.inverse()
+    tf_identical = chain_transform(tf_a2c, tf_c2a)
+    np.testing.assert_almost_equal(tf_identical.trans, np.zeros(3), decimal=8)
+    np.testing.assert_almost_equal(tf_identical.rot, np.eye(3), decimal=8)
