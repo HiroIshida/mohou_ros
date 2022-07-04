4040c9419075c8f367a2f3731707f9441ab1e8a1
HiroIshida <spitfire.docomo@gmail.com>
2022-07-04 21:15:03 +0900

    temp fix

--- mohou_ros_utils/pr2/executor.py
+++ mohou_ros_utils/pr2/executor.py
@@ -10,14 +10,19 @@ from mohou_ros_utils.executor import ExecutorBase
 from mohou_ros_utils.pr2.controller_utils import check_pr2_is_executable
 
 
+class RarmInterface(PR2ROSRobotInterface):
+    def default_controller(self):
+        return [self.rarm_controller, self.torso_controller, self.head_controller]
+
+
 class SkrobotPR2Executor(ExecutorBase):
     robot_model: PR2
-    robot_interface: PR2ROSRobotInterface
+    robot_interface: RarmInterface
 
     def post_init_hook(self):
         robot_model = PR2()
         self.robot_model = robot_model
-        self.robot_interface = PR2ROSRobotInterface(robot_model)
+        self.robot_interface = RarmInterface(robot_model)
         self.robot_model.angle_vector(self.robot_interface.angle_vector())
         check_pr2_is_executable()
 
