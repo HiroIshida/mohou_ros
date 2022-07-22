6b6852d3ef31651a471fc1b9fe2172f5f8fb2dad
HiroIshida <spitfire.docomo@gmail.com>
2022-07-22 22:59:18 +0900

    Fix rostest

--- rostest/run_pipeline.py
+++ rostest/run_pipeline.py
@@ -1,54 +1,67 @@
 #!/usr/bin/env python3
 import subprocess
+import unittest
 
 import gdown
+import rospy
 from mohou.file import get_project_path
 
-
-def drive_url(file_id):
-    url = "https://drive.google.com/uc?id={}".format(file_id)
-    return url
+import rostest
 
 
-# prepare rosbag
-project_name = "_mohou_ros_utils_test"
-project_path = get_project_path(project_name)
-project_path.mkdir(exist_ok=True)
+class TestNode(unittest.TestCase):
+    project_name = "_mohou_ros_utils_test"
 
-url = drive_url("18EtBZHK1SIxgGMKOrITd_Eg5MK3KnQXA")
-zip_path = project_path / "rosbag.zip"
-gdown.download(url=url, output=str(zip_path), quiet=False)
+    def setUp(self):
 
-rosbag_path = project_path
-rosbag_path.mkdir(exist_ok=True)
-subprocess.call("unzip -o {} -d {}".format(zip_path, rosbag_path), shell=True)
+        rospy.loginfo("prepare rosbag")
 
-# prepare config files
-url = drive_url("1LAF9JklX0NLUK_3isQOyFjNHSj-UzzWI")
-image_config_path = project_path / "image_config.yaml"
-gdown.download(url=url, output=str(image_config_path), quiet=True)
+        def drive_url(file_id):
+            url = "https://drive.google.com/uc?id={}".format(file_id)
+            return url
 
-url = drive_url("1_d2ijjxXTzmsfADccuwY2t8DqaYC6OyK")
-main_config_path = project_path / "main_config.yaml"
-gdown.download(url=url, output=str(main_config_path), quiet=True)
+        # prepare rosbag
+        project_path = get_project_path(self.project_name)
+        project_path.mkdir(exist_ok=True)
 
-# run commands
-subprocess.run(
-    "rosrun mohou_ros bags2chunk.py -hz 5 -remove_policy donothing -pn {}".format(project_name),
-    shell=True,
-)
+        url = drive_url("18EtBZHK1SIxgGMKOrITd_Eg5MK3KnQXA")
+        zip_path = project_path / "rosbag.zip"
+        gdown.download(url=url, output=str(zip_path), quiet=False)
 
-subprocess.run(
-    "rosrun mohou_ros bags2chunk.py -hz 20 -postfix autoencoder -remove_policy remove -pn {}".format(
-        project_name
-    ),
-    shell=True,
-)
+        rosbag_path = project_path
+        rosbag_path.mkdir(exist_ok=True)
+        subprocess.call("unzip -o {} -d {}".format(zip_path, rosbag_path), shell=True)
 
-print("train ae and lstm with n_epoch = 1")
-n_epoch_ae = 1
-n_epoch_lstm = 1
-subprocess.run(
-    "rosrun mohou_ros train {} {} {}".format(project_name, n_epoch_ae, n_epoch_lstm),
-    shell=True,
-)
+        # prepare config files
+        rospy.loginfo("prepare configs")
+        url = drive_url("1LAF9JklX0NLUK_3isQOyFjNHSj-UzzWI")
+        image_config_path = project_path / "image_config.yaml"
+        gdown.download(url=url, output=str(image_config_path), quiet=True)
+
+        url = drive_url("1_d2ijjxXTzmsfADccuwY2t8DqaYC6OyK")
+        main_config_path = project_path / "main_config.yaml"
+        gdown.download(url=url, output=str(main_config_path), quiet=True)
+
+    def test_pipeline(self):
+
+        # run commands
+        cmd = "rosrun mohou_ros bags2chunk.py -hz 5 -remove_policy donothing -pn {}".format(
+            self.project_name
+        )
+        rospy.loginfo(cmd)
+        subprocess.run(cmd, shell=True)
+
+        cmd = "rosrun mohou_ros bags2chunk.py -hz 20 -postfix autoencoder -remove_policy remove -pn {}".format(
+            self.project_name
+        )
+        rospy.loginfo(cmd)
+        subprocess.run(cmd, shell=True)
+
+        cmd = "rosrun mohou_ros train {} 1 1".format(self.project_name)
+        rospy.loginfo(cmd)
+        subprocess.run(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    rospy.init_node("test_sample")
+    rostest.rosrun("mohou_ros", "test_node", TestNode)
