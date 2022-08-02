812169714ae59f397512b3e125c73066cd6dc71d
HiroIshida <spitfire.docomo@gmail.com>
2022-08-03 04:35:43 +0900

    fix: no data augmentation is rostest to avoid svd error

--- scripts/train.py
+++ scripts/train.py
@@ -13,16 +13,23 @@ def train_and_visualize(
     n_epoch_lstm: int,
     n_hidden: int = 200,
     n_layer: int = 2,
+    n_aug_vae: int = 2,
+    n_aug_lstm: int = 19,
     valid_ratio: float = 0.1,
 ):
-    cmd_train_vae = "python3 -m mohou.script.train_autoencoder -pp {pp} -n {n} -image RGBImage -latent {n_latent} --vae -bundle_postfix autoencoder -valid-ratio {vr}".format(
-        pp=project_path, n=n_epoch_vae, n_latent=16, vr=valid_ratio
+    cmd_train_vae = "python3 -m mohou.script.train_autoencoder -pp {pp} -n {n} -image RGBImage -latent {n_latent} --vae -bundle_postfix autoencoder -valid-ratio {vr} -aug {aug}".format(
+        pp=project_path, n=n_epoch_vae, n_latent=16, vr=valid_ratio, aug=n_aug_vae
     )
     cmd_visualize_vae = "python3 -m mohou.script.visualize_autoencoder_result -pp {pp}".format(
         pp=project_path
     )
-    cmd_train_lstm = "python3 -m mohou.script.train_lstm -pp {pp} -n {n} -aug 19 -hidden {hidden} -layer {layer} -valid-ratio {vr}".format(
-        pp=project_path, n=n_epoch_lstm, hidden=n_hidden, layer=n_layer, vr=valid_ratio
+    cmd_train_lstm = "python3 -m mohou.script.train_lstm -pp {pp} -n {n} -hidden {hidden} -layer {layer} -valid-ratio {vr} -aug {aug}".format(
+        pp=project_path,
+        n=n_epoch_lstm,
+        hidden=n_hidden,
+        layer=n_layer,
+        vr=valid_ratio,
+        aug=n_epoch_lstm,
     )
     cmd_history = "python3 -m mohou.script.visualize_train_history -pp {}".format(project_path)
     cmd_visualize_lstm = "python3 -m mohou.script.visualize_lstm_result -pp {}".format(project_path)
@@ -56,6 +63,8 @@ if __name__ == "__main__":
     project_path = get_project_path(project_name)
     if is_testing:
         # as fast as possible. run only by rostest
-        train_and_visualize(project_path, 1, 1, n_hidden=1, n_layer=1, valid_ratio=0.5)
+        train_and_visualize(
+            project_path, 1, 1, n_hidden=1, n_layer=1, valid_ratio=0.5, n_aug_vae=0, n_aug_lstm=0
+        )
     else:
         train_and_visualize(project_path, n_epoch_vae, n_epoch_lstm, n_hidden=400, n_layer=2)
