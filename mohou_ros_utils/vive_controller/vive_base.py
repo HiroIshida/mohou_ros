b4787cf7c44f0bdbadd4fc1266d13b0cac3c8609
HiroIshida <spitfire.docomo@gmail.com>
2022-08-04 22:51:47 +0900

    refactor: vive classes

---
+++ mohou_ros_utils/vive_controller/vive_base.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+import time
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Callable, Generic, List, Optional, Type, TypeVar
+
+import genpy
+import rospy
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Joy
+
+from mohou_ros_utils.config import Config
+
+MessageT = TypeVar("MessageT", bound=genpy.Message)
+
+
+class TopicDataManager(ABC, Generic[MessageT]):
+    name: str
+    ttype: Type[MessageT]
+    subscriber: rospy.Subscriber
+    msg: Optional[MessageT] = None
+
+    def __init__(self, name, ttype):
+        # auto create subscriber
+        self.name = name
+        self.ttype = ttype
+        self.subscriber = rospy.Subscriber(name, ttype, self.callback)  # type: ignore
+
+    def callback(self, msg: MessageT):
+        self.msg = msg
+
+
+class PoseDataManager(TopicDataManager[PoseStamped]):
+    processor: Optional[Callable] = None
+
+    def __init__(self, name):
+        super().__init__(name, PoseStamped)
+
+    def register_processor(self, func: Callable):
+        self.processor = func
+
+    def process(self) -> None:
+        if self.processor is None:
+            return
+        self.processor()
+
+
+class JoyDataManager(TopicDataManager[Joy]):
+    class Button(Enum):
+        FRONT = 0
+        BOTTOM = 1
+        TOP = 2
+        SIDE = 3
+
+    trigger_times: List[Optional[float]]
+    latest_process_times: List[Optional[float]]
+    processors: List[Optional[Callable]]
+    min_trigger_interval: float = 0.1
+    min_process_interval: float = 0.3
+
+    def __init__(self, name):
+        super().__init__(name, Joy)
+        self.trigger_times = [None for _ in range(4)]
+        self.latest_process_times = [None for _ in range(4)]
+        self.processors = [None for _ in range(4)]
+
+    def callback(self, msg: Joy):
+        t = msg.header.stamp.to_sec()
+        for i in range(4):
+            if msg.buttons[i] == 1:
+                self.trigger_times[i] = t
+
+    def is_recently_processed(self, button_type: Button) -> bool:
+        latest_process_time = self.latest_process_times[button_type.value]
+        if latest_process_time is None:
+            return False
+        current_time = rospy.Time.now().to_sec()
+        return (current_time - latest_process_time) < self.min_process_interval
+
+    def is_recently_triggered(self, button_type: Button) -> bool:
+        trigger_time = self.trigger_times[button_type.value]
+        if trigger_time is None:
+            return False
+        current_time = rospy.Time.now().to_sec()
+        return (current_time - trigger_time) < self.min_trigger_interval
+
+    def register_processor(self, button: Button, processor: Callable) -> None:
+        self.processors[button.value] = processor
+
+    def process(self) -> None:
+        for button in self.Button:
+            func = self.processors[button.value]
+            if func is None:
+                continue
+            if not self.is_recently_triggered(button):
+                continue
+            if self.is_recently_processed(button):
+                continue
+            func()
+            self.latest_process_times[button.value] = rospy.Time.now().to_sec()
+
+
+class ViveController(ABC):
+    joy_manager: JoyDataManager
+    pose_manager: PoseDataManager
+    scale: float
+    timer_interval: float
+    is_initialized: bool
+    is_tracking: bool
+
+    def __init__(self, config: Config, scale: float, joy_topic_name: str, pose_topic_name: str):
+        self.joy_manager = JoyDataManager(joy_topic_name)
+        self.pose_manager = PoseDataManager(pose_topic_name)
+        self.scale = scale
+        self.timer_interval = 0.05
+
+        rospy.Timer(rospy.Duration(self.timer_interval), self.on_timer)
+        self.is_initialized = False
+        self.is_tracking = False
+        self.post_init_hook(config)
+
+    @abstractmethod
+    def post_init_hook(self, config: Config) -> None:
+        pass
+
+    def on_timer(self, event):
+        t_start = time.time()
+
+        if not self.is_initialized:
+            return
+        self.joy_manager.process()
+
+        if self.is_tracking:
+            self.pose_manager.process()
+
+        t_elapsed_in_cb = time.time() - t_start
+        process_time_rate = t_elapsed_in_cb / self.timer_interval
+        if process_time_rate > 0.5:
+            rospy.logwarn(
+                "take {:.2f} sec ({:.1f} %) of callback duration {:.2f} sec".format(
+                    t_elapsed_in_cb, process_time_rate * 100, self.timer_interval
+                )
+            )
