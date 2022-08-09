d97021bcca1eeed9132646566c5987e271837dfa
HiroIshida <spitfire.docomo@gmail.com>
2022-08-10 08:52:23 +0900

    Use mohou_ros config to setup pybullet vive

--- scripts/pybullet/vive_controll_pybullet.py
+++ scripts/pybullet/vive_controll_pybullet.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+import argparse
 import time
-from typing import List
+from typing import List, Optional
 
 import pybullet as pb
 import pybullet_data
 import rospy
+from mohou.file import get_project_path
 from skrobot.models import PR2
 
+from mohou_ros_utils.config import Config
 from mohou_ros_utils.pr2.params import rarm_joint_names
 from mohou_ros_utils.vive_controller.robot_interface import SkrobotPybulletController
+from mohou_ros_utils.vive_controller.utils import detect_controller_ids
 from mohou_ros_utils.vive_controller.vive_base import ViveRobotController
 
 
@@ -24,8 +28,7 @@ class SkrobotPybulletControllerPR2Rarm(SkrobotPybulletController):
 
 
 class BulletViveController(ViveRobotController[SkrobotPybulletControllerPR2Rarm]):
-    def __init__(self, scale: float = 1.5):
-        controller_id = "LHR_FFC55FC2"
+    def __init__(self, controller_id: str, config: Config, scale: float):
         super().__init__(controller_id, scale)
 
         # initialize pybullet world
@@ -34,6 +37,7 @@ class BulletViveController(ViveRobotController[SkrobotPybulletControllerPR2Rarm]
         robot_con = SkrobotPybulletControllerPR2Rarm(robot_id, pb.GUI)
         self.robot_con = robot_con
         self.robot_con.update_real_robot(1, real_time=False)
+        self.config = config
 
         pb.setAdditionalSearchPath(pybullet_data.getDataPath())  # used by loadURDF
         pb.configureDebugVisualizer(pb.COV_ENABLE_GUI, 0)
@@ -59,7 +63,27 @@ class BulletViveController(ViveRobotController[SkrobotPybulletControllerPR2Rarm]
         assert False
 
 
-rospy.init_node("pybullet_controller")
-cont = BulletViveController()
-cont.start()
-time.sleep(100)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-pn", type=str, help="project name")
+    parser.add_argument("-scale", type=float, default=1.5, help="controller to real scaling")
+    args, unknown = parser.parse_known_args()
+
+    scale: float = args.scale
+    project_name: Optional[str] = args.pn
+
+    project_path = get_project_path(project_name)
+    config = Config.from_project_path(project_path)
+
+    while True:
+        controller_ids = detect_controller_ids()
+        print("waiting for vive controllers detected")
+        if len(controller_ids) > 0:
+            print("controllers: {} are detected".format(controller_ids))
+            break
+    controller_id = controller_ids[0]
+
+    rospy.init_node("pybullet_vive_mohou")
+    cont = BulletViveController(controller_id, config, 1.5)
+    cont.start()
+    time.sleep(100)
