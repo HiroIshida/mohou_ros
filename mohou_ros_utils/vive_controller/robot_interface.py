ae42647f3b8a2f4be4872bd94e3fe53e76e74da9
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2022-08-22 06:24:02 +0900

    faet: move baxter by sending euslisp source code directly (#52)
    * faet: add baxter vive interface
    
    * Command softhand gripper
    
    * Separate two arms

--- mohou_ros_utils/vive_controller/robot_interface.py
+++ mohou_ros_utils/vive_controller/robot_interface.py
@@ -6,12 +6,15 @@ from typing import ClassVar, Dict, List, Type, TypeVar
 
 import numpy as np
 import pybullet as pb
+import rospkg
 import rospy
 from skrobot.interfaces.ros import PR2ROSRobotInterface  # type: ignore
 from skrobot.model import Link, RobotModel
 from skrobot.models.pr2 import PR2
+from skrobot.models.urdf import RobotModelFromURDF
 
 from mohou_ros.srv import EuslispDirectCommand, EuslispDirectCommandResponse
+from mohou_ros_utils.baxter.params import BaxterLarmProperty, BaxterRarmProperty
 from mohou_ros_utils.pr2.params import PR2LarmProperty, PR2RarmProperty
 from mohou_ros_utils.utils import (
     CoordinateTransform,
@@ -247,6 +250,71 @@ class EuslispPR2RarmController(PR2RarmProperty, EuslispPR2Controller):
 class EuslispPR2LarmController(PR2LarmProperty, EuslispPR2Controller):
     def move_gripper(self, pos: float) -> None:
         self.proxy("""(send *ri* :move-gripper :larm {})""".format(pos))
+
+    def arm_name(self) -> str:
+        return "larm"
+
+
+class EuslispBaxterController(EuslispRobotController):
+    def __init__(self):
+        super().__init__()
+        rospack = rospkg.RosPack()
+        model_urdf_path = Path(rospack.get_path("baxter_description"))
+        baxter_urdf_path = model_urdf_path / "urdf" / "baxter.urdf"
+        self.robot_model = RobotModelFromURDF(urdf_file=str(baxter_urdf_path))
+
+    def eus_script_hook(self) -> str:
+        script = """
+        (load "package://eus_vive/euslisp/lib/baxter-interface.l")
+        (baxter-init :type :{arm_name}-controller)
+        """.format(
+            arm_name=self.arm_name()
+        )
+        return script
+
+    def eus_joint_name_list(self) -> List[str]:
+        return [
+            "head_pan",
+            "left_s0",
+            "left_s1",
+            "left_e0",
+            "left_e1",
+            "left_w0",
+            "left_w1",
+            "left_w2",
+            "right_s0",
+            "right_s1",
+            "right_e0",
+            "right_e1",
+            "right_w0",
+            "right_w1",
+            "right_w2",
+        ]
+
+    @abstractmethod
+    def arm_name(self) -> str:
+        pass
+
+
+class EuslispBaxterRarmController(BaxterRarmProperty, EuslispBaxterController):
+    def move_gripper(self, pos: float) -> None:
+        assert pos in [0.0, 1.0]
+        if pos == 0.0:
+            self.proxy("""(send *ri* :start-grasp :rarm)""")
+        else:
+            self.proxy("""(send *ri* :stop-grasp :rarm)""")
+
+    def arm_name(self) -> str:
+        return "rarm"
+
+
+class EuslispBaxterLarmController(BaxterLarmProperty, EuslispBaxterController):
+    def move_gripper(self, pos: float) -> None:
+        assert pos in [0.0, 1.0]
+        if pos == 0.0:
+            self.proxy("""(send *ri* :start-grasp :larm)""")
+        else:
+            self.proxy("""(send *ri* :stop-grasp :larm)""")
 
     def arm_name(self) -> str:
         return "larm"
