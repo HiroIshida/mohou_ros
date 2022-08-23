5f929d353ec2a76ffe6fc1721197e3f9c3115667
HiroIshida <spitfire.docomo@gmail.com>
2022-08-24 06:28:28 +0900

    Rename n_pb to n_pb_dim

--- scripts/train.py
+++ scripts/train.py
@@ -16,7 +16,7 @@ def train_and_visualize(
     n_aug_vae: int = 2,
     n_aug_lstm: int = 19,
     valid_ratio: float = 0.1,
-    n_pb: Optional[int] = None,
+    n_pb_dim: Optional[int] = None,
 ):
     cmd_train_vae = "python3 -m mohou.script.train_autoencoder -pp {pp} -n {n} -image RGBImage -latent {n_latent} --vae -bundle_postfix autoencoder -valid-ratio {vr} -aug {aug}".format(
         pp=project_path, n=n_epoch_vae, n_latent=16, vr=valid_ratio, aug=n_aug_vae
@@ -26,9 +26,9 @@ def train_and_visualize(
     )
 
     # create train command
-    if n_pb is not None:
+    if n_pb_dim is not None:
         lstm_train_script_name = "mohou.script.train_pblstm"
-        additional_option = "-pb {}".format(n_pb)
+        additional_option = "-pb {}".format(n_pb_dim)
     else:
         lstm_train_script_name = "mohou.script.train_lstm"
         additional_option = ""
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     parser.add_argument("-pn", type=str, help="project name")
     parser.add_argument("-n_vae", type=int, default=1500, help="number of epoch")
     parser.add_argument("-n_lstm", type=int, default=20000, help="number of epoch")
-    parser.add_argument("-n_pb", type=int, help="number of parametric bias dimension")
+    parser.add_argument("-n_pb_dim", type=int, help="number of parametric bias dimension")
     parser.add_argument("--test", action="store_true", help="test mode")
 
     args = parser.parse_args()
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     n_epoch_vae: int = args.n_vae
     n_epoch_lstm: int = args.n_lstm
     is_testing: bool = args.test
-    n_pb: Optional[int] = args.n_pb
+    n_pb_dim: Optional[int] = args.n_pb_dim
 
     project_path = get_project_path(project_name)
     if is_testing:
@@ -88,9 +88,9 @@ if __name__ == "__main__":
             valid_ratio=0.5,
             n_aug_vae=0,
             n_aug_lstm=4,
-            n_pb=n_pb,
+            n_pb_dim=n_pb_dim,
         )
     else:
         train_and_visualize(
-            project_path, n_epoch_vae, n_epoch_lstm, n_hidden=400, n_layer=2, n_pb=n_pb
+            project_path, n_epoch_vae, n_epoch_lstm, n_hidden=400, n_layer=2, n_pb_dim=n_pb_dim
         )
