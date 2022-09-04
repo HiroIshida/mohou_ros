055472635dbfd407e0b50093b5e50321eb8d1b4e
HiroIshida <spitfire.docomo@gmail.com>
2022-09-05 03:11:24 +0900

    feat: check if reset home position completed

--- scripts/pr2/reset_to_home.py
+++ scripts/pr2/reset_to_home.py
@@ -2,11 +2,13 @@
 import argparse
 from typing import Optional
 
+import rospy
 from mohou.file import get_project_path
 from skrobot.interfaces.ros import PR2ROSRobotInterface  # type: ignore
 from skrobot.models import PR2
 
 from mohou_ros_utils.config import Config
+from mohou_ros_utils.utils import check_home_position_consistensy
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -31,3 +33,8 @@ if __name__ == "__main__":
     ri.move_gripper("larm", config.home_position["l_gripper_joint"], effort=100)
     ri.move_gripper("rarm", config.home_position["r_gripper_joint"], effort=100)
     ri.wait_interpolation()
+
+    exclude_keywords = ["gripper", "laser_tilt", "caster", "motor_screw"]
+    check_home_position_consistensy(ri, config, exclude_keywords)
+    rospy.loginfo("reset to home position successful")
+    assert False
